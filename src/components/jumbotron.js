3eac94c171a63708955e57c139427d73a0425746
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-07 14:09:22 +0100

    Adding props

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -2,13 +2,17 @@ import React from "react";
 import Search from "./search";
 
 
-function Jumbotron() {
+function Jumbotron(props) {
     return (
         <div className="p-5 m-4  bg-info border rounded-3">
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">Employee Directory</h1>
                 <p className="col-md-8 fs-4">Click on the columns labels to filter by heading or use the search box to narrow your results.</p>
-             <Search />
+                <Search
+                    search={props.search}
+                    handleFormSubmit={props.handleFormSubmit}
+                    handleInputChange={props.handleInputChange}
+                />
             </div>
         </div>
 
