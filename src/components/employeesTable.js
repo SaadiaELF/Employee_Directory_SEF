a6d056d927fa00c1b4738b316b6e7ff498385bca
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-07 11:11:25 +0100

    Adding props and map

--- src/components/employeesTable.js
+++ src/components/employeesTable.js
@@ -1,16 +1,18 @@
 import React from "react";
+
 const styles = {
     tableStyle: {
-      width : '90%'
+        width: '90%'
     }
-  };
-function EmployeesTable() {
+};
+
+function EmployeesTable(props) {
     return (
         <div>
             <div className="table-responsive ">
                 <table className="table align-middle table-hover table-bordered border-info mx-auto" style={styles.tableStyle}>
                     <thead>
-                        <tr  className="table-info">
+                        <tr className="table-info">
                             <th scope="col">Name</th>
                             <th scope="col">Photo</th>
                             <th scope="col">Date of Birth</th>
@@ -20,14 +22,16 @@ function EmployeesTable() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
+                        {props.results.map(result => (
+                            <tr>
+                                <th scope="row">{result.name.first + result.name.last}</th>
+                                <td><img alt={result.name.first + result.name.last} className="img-fluid" src={result.picture.thumbnail} /></td>
+                                <td>{result.dob.date}</td>
+                                <td>{result.location.city}</td>
+                                <td>{result.email}</td>
+                                <td>{result.cell}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </div>
