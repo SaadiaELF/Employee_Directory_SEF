336756e096a3113ab4e06b7815e8090359268ae7
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-07 12:51:30 +0100

    Creating a search component

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,10 +1,6 @@
 import React from "react";
+import Search from "./search";
 
-const styles = {
-    inputStyle: {
-      width : '30%'
-    }
-  };
 
 function Jumbotron() {
     return (
@@ -12,11 +8,7 @@ function Jumbotron() {
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">Employee Directory</h1>
                 <p className="col-md-8 fs-4">Click on the columns labels to filter by heading or use the search box to narrow your results.</p>
-                <div className="input-group mb-3 position-absolute start-30 end-30 " style={styles.inputStyle}>
-                    <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon">
-                    </input>
-                    <button className="btn btn-outline-dark" type="button" id="button-addon">Search</button>
-                </div>
+             <Search />
             </div>
         </div>
 
