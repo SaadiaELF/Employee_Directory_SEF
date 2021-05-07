5532c03736710b002daaade799a3deda44fff8fb
Saadia El fekak <saadiaelfekak@gmail.com>
2021-05-07 14:11:19 +0100

    Adding props

--- src/components/search.js
+++ src/components/search.js
@@ -1,19 +1,26 @@
 import React from "react";
 
 const styles = {
-    inputStyle: {
-      width : '30%'
-    }
-  };
+  inputStyle: {
+    width: '30%'
+  }
+};
 
-function Search() {
-    return (
-        <div className="input-group mb-3 position-absolute start-30 end-30 " style={styles.inputStyle}>
-            <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon">
-            </input>
-            <button className="btn btn-outline-dark" type="button" id="button-addon">Search</button>
-        </div>
-    );
+function Search(props) {
+  return (
+    <div className="input-group mb-3 position-absolute start-30 end-30 " style={styles.inputStyle}>
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby="button-addon" />
+      <button onClick={props.handleFormSubmit} className="btn btn-outline-dark" type="button" id="button-addon">Search</button>
+    </div>
+  );
 }
 
 export default Search;
